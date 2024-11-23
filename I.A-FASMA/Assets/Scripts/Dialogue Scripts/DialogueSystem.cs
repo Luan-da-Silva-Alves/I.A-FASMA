f9cf08d1cd95630246114a2e54bb8629fef137e9
Luan-da-Silva-Alves <luansilvalves3@hotmail.com>
2024-11-23 20:00:21 -0300

    Perguntas feitas e história completa
    história completa, as perguntas e sistema de pontuação estão feitos, agora falta os caminhos extras caso o jogador escolha as opções erradas

--- I.A-FASMA/Assets/Scripts/Dialogue Scripts/DialogueSystem.cs
+++ I.A-FASMA/Assets/Scripts/Dialogue Scripts/DialogueSystem.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class DialogueSystem : MonoBehaviour
 {
+    public Text scoreText;
     public Text dialogueText; // O texto que será exibido na tela
     public GameObject[] dialogueOptions; // Opções de respostas do diálogo
     public string[] dialogueLines; // As linhas de diálogo
     private int currentLine = 0; // Linha atual do diálogo
     public RawImage ghost;
     public Texture2D[] bgGhost;
-
+    private int playerScore = 50;
+    private const int WinningScore = 150; // Pontuação para vencer o jogo
+    private const int LosingScore = 0; // Pontuação mínima antes de perder
 
 
 
@@ -30,23 +34,42 @@ public class DialogueSystem : MonoBehaviour
             "É claro que não! a história é importante, pois você tera que responder algumas perguntas que teram haver com ela.", // Linha 4
             "Olha não sou eu que faz as regras eu apenas as sigo.", // Linha 5
             "Á muito tempo, no alto de uma colina cercada por neblina constante, existia uma mansão antiga, habitada apenas por um espírito conhecido como Fasmo. Ele era um fantasma solitário, vagando pelos corredores empoeirados e ouvindo os ecos de uma vida que já não existia.", // Linha 6
+            
+            "Durante o dia, a mansão permanecia em silêncio absoluto, mas à noite, Fasmo preenchia os cômodos com sons de passos e sussurros. Ele tinha um segredo: em vida, havia sido um músico talentoso que ninguém nunca ouviu, e no salão principal da mansão,solitario, repousava um velho piano de cauda, onde Fasmo tocava melodias tristes para a lua cheia.", // Linha 7
+            "Depois de anos solatarios fasmo veio a falecer, sozinho após anos de isolamento", //linha 8
+            "Certo dia, um grupo de jovens corajosos decidiu explorar a mansão, em busca de aventuras e lendas. Ao entrarem, ouviram uma música melancólica ecoando pelos corredores. Intrigados, seguiram o som até o salão principal, onde encontraram Fasmo ao piano..", // Linha 9
+            "Para sua surpresa, ele não era um espírito vingativo, mas sim um fantasma amigável, que ansiava por companhia e por alguém que ouvisse suas composições.", // Linha 10
+            "A partir daquela noite, a mansão deixou de ser um lugar assustador e tornou-se um refúgio para os jovens, que passaram a se reunir ali para ouvir Fasmo tocar e aprender sobre sua vida misteriosa.\r\n\r\n", // Linha 11
+            "Agora,as perguntas.", // Linha 12
+            "Qual o segredo de Fasmo?.", // Linha 13 pergunta 1
+            "Certa resposta", // Linha 14
+
+            "Facil é? certo, por que Fasmo tocava melodias tristes no piano?.", // Linha 15 pergunta 2
+            "Isso mesmo!", // Linha 16
+
+            "O que motivou os jovens a explorar a mansão?", // Linha 17 pergunta 3
+            "Ai sim! você está indo muito bem", // Linha 18
+            "Como Fasmo morreu e se tornou um fantasma?", // Linha 19 pergunta 4
+            "Algum outro espírito habitava a mansão, ou Fasmo era o único?", // Linha 20 pergunta 5
+            "Por que Fasmo tocava melodias tristes no piano?",// linha 21 pergunta 6
+            "Ta se achando o novo Socrates é?", //linha22
+            "Carai borracha..., enfim, como os jovens reagiram ao encontrar Fasmo pela primeira vez?",// linha 23 pergunta 7
+            "Isso eu...digo ele nunca faria mal a ninguém",// linha 24
+
+            "A mansão tinha outros segredos além de Fasmo?",// linha 25 pergunta 8
+            "Oque quer dizer com isso?",//linha 26
+            "Chata oq? eu não sei oq essas palavras significam, GPT? é um tipo de clã maliguino?",//linha 27
+            "Ah, enfim próxima pergunta ",//linha 28
+            "Havia algo especial ou mágico no piano de Fasmo?",//29 perunta 9
+            "Você acha que a  história de Fasmo inspirou os jovens de alguma maneira?",//linha 30 perunta 10
+            "Fasmo tinha alguma ligação com a construção ou o passado da mansão?",//linha 31 pergunta 11
+            
 
 
-            "Durante o dia, a mansão permanecia em silêncio absoluto, mas à noite, Fasmo preenchia os cômodos com sons de passos e sussurros. Ele tinha um segredo: em vida, havia sido um músico talentoso, e no salão principal da mansão repousava um velho piano de cauda, onde Fasmo tocava melodias tristes para a lua cheia.", // Linha 7
-            "Certo dia, um grupo de jovens corajosos decidiu explorar a mansão, em busca de aventuras e lendas. Ao entrarem, ouviram uma música melancólica ecoando pelos corredores. Intrigados, seguiram o som até o salão principal, onde encontraram Fasmo ao piano..", // Linha 8
-            "Para sua surpresa, ele não era um espírito vingativo, mas sim um fantasma amigável, que ansiava por companhia e por alguém que ouvisse suas composições.", // Linha 9
-            "A partir daquela noite, a mansão deixou de ser um lugar assustador e tornou-se um refúgio para os jovens, que passaram a se reunir ali para ouvir Fasmo tocar e aprender sobre sua vida misteriosa.\r\n\r\n", // Linha 10
-            "Agora,as perguntas.", // Linha 11
-            "Qual o segredo de Fasmo?.", // Linha 12
-            "Certa resposta", // Linha 13
 
-            "Facil é? certo, por que Fasmo tocava melodias tristes no piano?.", // Linha 14
-            "Isso mesmo!", // Linha 15
 
-            "O que motivou os jovens a explorar a mansão?", // Linha 16
-            "Espero que tenha gostado de ouvir as histórias. Até logo!", // Linha 17
-            "Errou!", // Linha 18
-            "Se decidir voltar, estarei aqui para compartilhar mais aventuras.", // Linha 19
+
+
 
 
 
@@ -65,16 +88,34 @@ public class DialogueSystem : MonoBehaviour
             "Certo", // Linha 7
             "Ok...", // Linha 8
             "Hum...", // Linha 9
-            "Certo!", // Linha 10
-            "Cero,Manda ver!", // Linha 11
-            "Ele gostava de tocar piano, porém ele sempre fez isso sozinho ", // Linha 12
-            "Essa foi facil!", // Linha 13
-            "Pois ele vivia sozinho", // Linha 14
-            "Vai tarde", // Linha 15
-            "Até mais!", // Linha 16
-            "Que nada, só fazer um pix de R$100", // Linha 17
-            "Até breve biruta", // Linha 17
+            "Certo", // Linha 10
+            "Certo!", // Linha 11
+            "Cero,Manda ver!", // Linha 12
+            "Ele gostava de tocar piano, porém ele sempre fez isso sozinho ", // Linha 13
+            "Essa foi facil!", // Linha 14
+            "Pois ele vivia sozinho", // Linha 15
+            "Viva", // Linha 16
+            "Eles foram em busca de aventuras e lendas", // Linha 17
+            "É claro, eu estava ouvindo a história", // Linha 18
+            "Fasmo morreu de um coração partido, sozinho após anos de isolamento,sua forte ligação emocional com a música ", // Linha 19
+            "Ele era o unico",//linha 20
+            "As melodias refletiam sua solidão e arrependimento",//linha 21
+            "Nhe nhe nhe, quer que eu te liberte ou não?",//linha 22
+            "Eles ficaram com medo, porém depois viram que ele era um fantasma amigavel",//linha 23
+            "Certo, certo",//linha 24
+            "Não, você não falou nada sobre isso, ta delirando?",//linha 25
+            "Nada não é que até parece que você usou chat gpt para gerar essas perguntas",//linha 26
+            "Quase isso...",//linha 27
+            "Ok",//linha28
+            "Não, ele um piano comum apenas as musicas eram 'magicas'",//linha 29
+            "Com certeza, principalmente a nunca abandonar os amigos, vai que ele é um excelente pianista!",//linha 30
+            "Sim, a mansão era dele pelo que deu a entender",//linha 31
             
+
+
+
+
+
         };
 
         negativeLabels = new string[] {
@@ -143,19 +184,47 @@ public class DialogueSystem : MonoBehaviour
         {
             case 0: // Primeira linha de diálogo
             case 1:
-                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
-                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora."
+                dialogueOptions[0].SetActive(true); 
+                dialogueOptions[1].SetActive(true); 
                 break;
-           case 12: // Primeira decisão antes das perguntas
-                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
-                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora."
+           case 13: // Primeira decisão antes das perguntas
+                dialogueOptions[0].SetActive(true); 
+                dialogueOptions[1].SetActive(true); 
                 break;
-            case 14: // Quando o jogador pergunta sobre os portais
-                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+            case 15: // Quando o jogador pergunta sobre os portais
+                dialogueOptions[0].SetActive(true); 
                 dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora."
                 break;
 
-            case 16: // Caso o diálogo chegue ao fim
+            case 17: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 19: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 21: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 23: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 25: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 29: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 30: // Caso o diálogo chegue ao fim
+                dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
+                dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
+                break;
+            case 31: // Caso o diálogo chegue ao fim
                 dialogueOptions[0].SetActive(true); // "Sim, me conte mais!"
                 dialogueOptions[1].SetActive(true); // "Não, prefiro ir embora
                 break;
@@ -188,27 +257,76 @@ public class DialogueSystem : MonoBehaviour
     {
         switch (currentLine)
         {
-            case 0: // Introdução
-            case 1: // Opção inicial de saber mais
+          
+
+            case 13: // P1
+                currentLine = 14; 
+                UpdateScoreText();
+                AddPoints(10);
                 int indexImageFeliz = 4;
                 ChangeSprite(indexImageFeliz);
-                currentLine = 2; // Avança para o Reino dos Dragões
                 break;
 
-            case 11: // Quando o jogador escolhe saber mais sobre dragões
-                currentLine = 12; // Fala mais sobre os dragões
+            case 15: //P2
+                currentLine = 16; 
+                AddPoints(10);
                 break;
 
-            case 14: // Quando o jogador escolhe saber mais sobre os portais
-                currentLine = 13; // Fala mais sobre os portais
+                case 17: // P3
+                currentLine = 18; 
+                AddPoints(10);
+                break;
+            case 19: // P4
+                currentLine = 20; // Fala mais sobre os portais
+                AddPoints(10);
+                break;
+            case 20: // P5
+                currentLine = 21; // Fala mais sobre os portais
+                AddPoints(10);
+                int indexImageDesconfiado = 1;
+                ChangeSprite(indexImageDesconfiado);
+
+                break;
+            case 21: // P5
+                currentLine = 22; // Fala mais sobre os portais
+                AddPoints(10);
+                break;
+            case 23: // P6
+                currentLine = 24; // Fala mais sobre os portais
+                AddPoints(10);
+                break;
+            case 25: // P8
+                currentLine = 26; // Fala mais sobre os portais
+                
+                break;
+            case 27:
+                int indexImagePreocupado = 5;
+                ChangeSprite(indexImagePreocupado);
+                currentLine = 28;
+                break;
+            case 29: // P9
+                currentLine = 30; // Fala mais sobre os portais
+                AddPoints(10);
+                break;
+
+            case 30: // P10
+                currentLine = 31; // Fala mais sobre os portais
+                AddPoints(10);
+                int indexImageDesconfiado2 = 2;
+                ChangeSprite(indexImageDesconfiado2);
+                break;
+            case 31: // P11
+                currentLine = 32; // Fala mais sobre os portais
+                AddPoints(10);
                 break;
 
             default: // Para todas as outras opções
                 currentLine += 1; // Apenas avança para a próxima linha
-                int indexImagePadrao = 3;
+                int indexImagePadrao = 4;
                 ChangeSprite(indexImagePadrao);
                 break;
         }
+
     }
 
     // Lógica quando o jogador escolhe "Não, prefiro ir embora."
@@ -216,26 +334,58 @@ public class DialogueSystem : MonoBehaviour
     {
         switch (currentLine)
         {
-            case 0: // Introdução
-            case 12: // Opção inicial de saber mais
-                int indexImage = 0;
-                ChangeSprite(indexImage);
-                currentLine = 18; //texto de erro 
+
+
+            case 13: // P1
+                currentLine = 14;
+                UpdateScoreText();
+                AddPoints(10);
+               
                 break;
 
-            case 18: // Quando o jogador escolhe saber mais sobre dragões
-                currentLine = 12; // Fala mais sobre os dragões
+            case 15: //P2
+                currentLine = 16;
+                SubtractPoints(10);
                 break;
 
-            case 14: // Quando o jogador escolhe saber mais sobre os portais
-                currentLine = 13; // Fala mais sobre os portais
+            case 17: // P3
+                currentLine = 18;
+                SubtractPoints(10);
+                break;
+            case 19: // P4
+                currentLine = 20; // Fala mais sobre os portais
+                SubtractPoints(10);
+                break;
+            case 20: // P5
+                currentLine = 21; // Fala mais sobre os portais
+                SubtractPoints(10);
+
+                break;
+            case 21: // P5
+                currentLine = 22; // Fala mais sobre os portais
+                SubtractPoints(10);
+                break;
+            case 23: // P6
+                currentLine = 24; // Fala mais sobre os portais
+                SubtractPoints(10);
+                break;
+            case 25: // P8
+                currentLine = 34; // Fala mais sobre os portais
+                SubtractPoints(10);
+                break;
+            case 29: // P9
+                currentLine = 30; // Fala mais sobre os portais
+                SubtractPoints(10);
                 break;
 
-            default: // Para todas as outras opções
-                currentLine += 1; // Apenas avança para a próxima linha
-                int indexImagePadrao = 3;
-                ChangeSprite(indexImagePadrao);
+            case 30: // P10
+                currentLine = 31; // Fala mais sobre os portais
+              SubtractPoints(10);
                 break;
+            case 31: // P11
+                currentLine = 32; // Fala mais sobre os portais
+                SubtractPoints(10);
+                break; 
         }
         int indexImageBravo = 0;
         ChangeSprite(indexImageBravo);
@@ -250,7 +400,7 @@ public class DialogueSystem : MonoBehaviour
     // Finaliza o diálogo
     void EndDialogue()
     {
-        dialogueText.text = "O diálogo terminou. Você pode voltar quando quiser!";
+        dialogueText.text = "Você Conseguiu obrigado por me libertar!";
         foreach (GameObject option in dialogueOptions)
         {
             option.SetActive(false); // Aqui os botões são desativados quando o diálogo termina
@@ -261,4 +411,56 @@ public class DialogueSystem : MonoBehaviour
         //Debug.Log("Change Sprite");
         ghost.texture = bgGhost[indexImage];
     }
+    public void UpdateScoreText()
+    {
+        scoreText.text = "Pontuação: " + playerScore;
+    }
+    public void AddPoints(int points)
+    {
+        playerScore += points;
+        UpdateScoreText();
+        CheckWinCondition();
+    }
+    public void SubtractPoints(int points)
+    {
+        playerScore -= points;
+        UpdateScoreText();
+        CheckLoseCondition();
+    }
+
+    // Verifica se o jogador venceu
+    private void CheckWinCondition()
+    {
+        if (playerScore >= WinningScore)
+        {
+            GameWin();
+            EndDialogue();
+            DisabledAllOptions();
+        }
+    }
+
+    // Verifica se o jogador perdeu
+    private void CheckLoseCondition()
+    {
+        if (playerScore <= LosingScore)
+        {
+            GameOver();
+            EndDialogue();
+        }
+    }
+
+    // Lógica de vitória
+    private void GameWin()
+    {
+        Debug.Log("Você venceu o jogo!");
+        EndDialogue();
+    }
+
+    // Lógica de derrota
+    private void GameOver()
+    {
+        Debug.Log("Você perdeu o jogo!");
+        // Adicione aqui o que acontecerá quando o jogador perder
+    }
 }
+
